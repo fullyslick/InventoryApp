0bb324ea2adf769aa003ddc25c3957e52ed43344
fullyslick <alexander.rashkov.alfa@gmail.com>
2017-07-10 16:31:11 +0300

    Създал съм Contract-а - CONTENT_URI, TABLE_NAME, COLUMNS

--- app/src/main/java/com/example/user/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/user/inventoryapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.user.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_main_activity.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                 // TO:DO INSERT METHOD HERE
+
+                return true;
+
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                // TO:DO howDeleteConfirmationDialog(); HERE
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/user/inventoryapp/data/ProductContract.java
@@ -0,0 +1,57 @@
+package com.example.user.inventoryapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Alexander Rashkov on 10.07.17.
+ */
+
+public class ProductContract {
+
+    // Declare "Content authority"
+    public static final String CONTENT_AUTHORITY = "com.example.user.inventoryapp";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    // Possible path (appended to base content URI for possible URI's)
+    public static final String PATH_PRODUCTS = "products";
+
+    // Preventing someone from accidentally instantiating the contract class,
+    // by giving it an empty constructor.
+    private ProductContract() {
+    }
+
+    public static abstract class ProductEntry implements BaseColumns {
+        /**
+         * The content URI to access the product data in the provider
+         * CONTENT_URI = content://com.example.user.inventoryapp/products
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+        // Name of database table for products
+        public static final String TABLE_NAME = "products";
+
+        // Unique ID number for the product (only for use in the database table).
+        public static final String _ID = BaseColumns._ID;
+
+        public static final String COLUMN_PRODUCT_NAME = "product_name";
+        public static final String COLUMN_PRODUCT_QUANTITY = "quantity";
+        public static final String COLUMN_PRODUCT_PRICE = "price";
+        public static final String COLUMN_PRODUCT_PHOTO_URI = "photo_uri";
+        public static final String COLUMN_SUPPLIER_NAME = "supplier_name";
+        public static final String COLUMN_SUPPLIER_EMAIL = "supplier_email";
+
+        // The MIME type of the {@link #CONTENT_URI} for a list of products.
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        // The MIME type of the {@link #CONTENT_URI} for a single product.
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+    }
+}
